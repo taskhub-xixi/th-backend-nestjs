ac3bc29ae1c503f51bcdd29bfbd092d066170005
saken78 <sakenspot@gmail.com>
2026-03-14 12:47:20 +0700

    refactor(auth): use new guards and remove manual validation logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,11 @@
 import {
-  BadRequestException,
   Body,
   Controller,
   Get,
   HttpCode,
   HttpException,
   HttpStatus,
+  Patch,
   Post,
   Put,
   Req,
@@ -17,91 +17,53 @@ import { Request, Response } from "express";
 import { WebResponse } from "src/model/web.mode";
 import {
   CookiePayload,
+  DeleteDTO,
   LoginDTO,
   RegisterDTO,
   UpdateDTO,
   UserResponse,
 } from "../model/auth.model";
 import { AuthService } from "./auth.service";
-import { LocalAuthGuard } from "./local-auth.guard";
-import { JwtStrategy } from "./strategies/jwt.strategy";
-import { LocalStrategy } from "./strategies/local.strategy";
+import { CheckUserGuard } from "./guards/check-user.guard";
+import { JwtAuthGuard } from "./guards/jwt-auth.guard";
 
 @Controller("/api/auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post("register")
-  @HttpCode(HttpStatus.OK)
+  @HttpCode(HttpStatus.CREATED)
   async register(
     @Body() registerDTO: RegisterDTO,
     @Res({ passthrough: true }) response: Response,
   ): Promise<WebResponse<UserResponse>> {
-    // validate
-    if (!registerDTO.username || registerDTO.username.trim() === "") {
-      throw new HttpException("username cannot be empty", 401);
-    }
-
-    if (!registerDTO.email || registerDTO.email.trim() === "") {
-      throw new HttpException("email cannot be empty", 401);
-    }
-
-    if (!registerDTO.password || registerDTO.password.trim() === "") {
-      throw new HttpException("password cannot be empty", 401);
-    }
-
-    if (registerDTO.password.length < 8) {
-      throw new HttpException("password must be atleast 8 charachter", 401);
-    }
-
-    if (registerDTO.username.length < 2) {
-      throw new HttpException("username must be atleast 2 charachter", 401);
-    }
-
-    if (registerDTO.email.length < 8) {
-      throw new HttpException("email must be atleast 8 charachter", 401);
-    }
-
     const result = await this.authService.create(registerDTO, response);
     return { data: result };
   }
 
-  @UseGuards(LocalStrategy)
+  @UseGuards(CheckUserGuard)
   @Post("login")
-  @HttpCode(200)
+  @HttpCode(HttpStatus.OK)
   async login(
     @Body() loginDTO: LoginDTO,
     @Res({ passthrough: true }) response: Response,
   ): Promise<WebResponse<UserResponse>> {
-    // validate
-    if (!loginDTO.email || loginDTO.email.trim() === "") {
-      throw new BadRequestException("email cannot be empty");
-    }
-    if (!loginDTO.password || loginDTO.password.trim() === "") {
-      throw new BadRequestException("password cannot be empty");
-    }
-    if (loginDTO.password.length < 8) {
-      throw new BadRequestException("password minimum is 8 charachter");
-    }
-    if (loginDTO.email.length < 8) {
-      throw new BadRequestException("email minimum is 8 charachter");
-    }
-
-    // validate user
-    const result = await this.authService.login(loginDTO);
+    const result = await this.authService.login(loginDTO, response);
 
     return {
       data: result,
     };
   }
 
+  @UseGuards(JwtAuthGuard)
+  @UseGuards(CheckUserGuard)
   @Post("refresh")
-  @HttpCode(200)
+  @HttpCode(HttpStatus.OK)
   async refresh(
     @Req() req: Request,
     @Res({ passthrough: true }) res: Response,
   ): Promise<void> {
-    const refreshToken = (req.cookies as CookiePayload)["refreshToken"];
+    const refreshToken = (req.cookies as CookiePayload)["refresh_token"];
 
     if (!refreshToken) {
       throw new UnauthorizedException();
@@ -109,106 +71,58 @@ export class AuthController {
 
     const tokens = await this.authService.refresh(refreshToken);
 
-    res.cookie("accessToken", tokens.accessToken);
-    res.cookie("refreshToken", tokens.refreshToken, {
+    res.cookie("access_token", tokens.accessToken);
+    res.cookie("refresh_token", tokens.refreshToken, {
       httpOnly: true,
       secure: true,
       sameSite: "strict",
     });
   }
 
-  @Put("reset")
-  @HttpCode(200)
+  @UseGuards(CheckUserGuard)
+  @UseGuards(JwtAuthGuard)
+  @Patch("reset")
+  @HttpCode(HttpStatus.OK)
   async resetPassword(@Body() resetDTO: UpdateDTO): Promise<void> {
     try {
-      if (!resetDTO.email || resetDTO.email.trim() === "") {
-        throw new BadRequestException("email cannot be empty");
-      }
-      if (!resetDTO.password || resetDTO.password.trim() === "") {
-        throw new BadRequestException("password cannot be empty");
-      }
-      if (!resetDTO.passwordChanged || resetDTO.passwordChanged.trim() === "") {
-        throw new BadRequestException("repeat password cannot be empty");
-      }
-      if (resetDTO.email.length < 8) {
-        throw new BadRequestException("email must be atleast 8 charachter");
-      }
-      if (resetDTO.password.length < 8) {
-        throw new BadRequestException("password must be atleast 8 charachter");
-      }
-      if (resetDTO.passwordChanged.length < 8) {
-        throw new BadRequestException(
-          "repeat password must be atleast 8 charachter",
-        );
-      }
-      if (resetDTO.password !== resetDTO.passwordChanged) {
-        throw new BadRequestException(
-          "password and repeat password is not the same",
-        );
-      }
+      if (!resetDTO.email) throw new HttpException("Something Wrong", 404);
+      if (!resetDTO.password) throw new HttpException("Something Wrong", 404);
       await this.authService.resetPassword(resetDTO.email, resetDTO.password);
     } catch (err: unknown) {
-      throw new BadRequestException(
-        (err as Error).message || "unknown message",
-      );
+      throw new HttpException((err as Error).message, 401);
     }
   }
 
+  @UseGuards(CheckUserGuard)
+  @UseGuards(JwtAuthGuard)
   @Put("delete")
+  @HttpCode(HttpStatus.OK)
   async delete(
-    @Body("email") email: string,
-    @Body("password") password: string,
-    @Body("confPassword") confPassword: string,
+    @Body() deleteDTO: DeleteDTO,
     @Res({ passthrough: true }) response: Response,
   ): Promise<void> {
-    if (!email || email.trim() === "") {
-      throw new BadRequestException("email cannot be empty");
-    }
-    if (!password || password.trim() === "") {
-      throw new BadRequestException("password cannot be empty");
-    }
-    if (password.length < 8) {
-      throw new BadRequestException("password must be atleast 8 charachter");
-    }
-    if (email.length < 8) {
-      throw new BadRequestException("email must be atleast 8 charachter");
-    }
-    if (!confPassword || confPassword.trim() === "") {
-      throw new BadRequestException("cofirmation password cannot be empty");
-    }
-    if (confPassword.length < 8) {
-      throw new BadRequestException(
-        "confirmation password must be atleast 8 charachter",
-      );
-    }
-    if (confPassword !== password) {
-      throw new BadRequestException(
-        "confirmation password and password is not same",
-      );
-    }
-    response.clearCookie("refreshToken");
-    response.clearCookie("accessToken");
-    await this.authService.deleteSQL(email);
+    await this.authService.deleteSQL(deleteDTO.email, response);
   }
 
+  @UseGuards(CheckUserGuard)
+  @UseGuards(JwtAuthGuard)
   @Get("verify")
-  @HttpCode(200)
+  @HttpCode(HttpStatus.OK)
   async verify(@Req() request: Request) {
-    // ambil cookie
+    // take cookie from browser
     const cookie = (request.cookies as CookiePayload).accessToken;
     return await this.authService.verify(cookie);
   }
 
-  @UseGuards(LocalAuthGuard)
-  @Put("logout")
-  @HttpCode(200)
+  @UseGuards(CheckUserGuard)
+  @UseGuards(JwtAuthGuard)
+  @Patch("logout")
+  @HttpCode(HttpStatus.OK)
   async logout(
-    @Body("email") email: string,
+    @Body() deleteDTO: DeleteDTO,
     @Res({ passthrough: true }) response: Response,
   ): Promise<{ message: string }> {
-    response.clearCookie("refresh_token");
-    response.clearCookie("access_token");
-    await this.authService.logout(email);
+    await this.authService.logout(deleteDTO.email, response);
     return {
       message: "clear Cookie",
     };
