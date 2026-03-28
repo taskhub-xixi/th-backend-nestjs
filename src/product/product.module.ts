e10078b1b40f84eb72b5a2aa30543233497b070f
saken78 <sakenspot@gmail.com>
2026-03-28 18:09:01 +0700

    refactor(module): clean up product module imports
    - Remove unused PassportModule import
    - Remove unused JwtStrategy and UserStrategy providers
    - Remove redundant AuthService provider (already in AuthModule)
    - Simplify imports to only include AuthModule

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,14 +1,11 @@
 import { Module } from "@nestjs/common";
 import { ProductController } from "./product.controller";
 import { ProductService } from "./product.service";
-import { AuthService } from "../auth/auth.service";
 import { AuthModule } from "../auth/auth.module";
-import { PassportModule } from "@nestjs/passport";
-import { JwtStrategy, UserStrategy } from "../common/strategies";
 
 @Module({
-  imports: [AuthModule, PassportModule],
+  imports: [AuthModule],
   controllers: [ProductController],
-  providers: [ProductService, AuthService, JwtStrategy, UserStrategy],
+  providers: [ProductService],
 })
 export class ProductModule {}
