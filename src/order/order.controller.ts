9524441a8b8b297394d908f0c42e948a252750c6
saken78 <sakenspot@gmail.com>
2026-03-29 19:42:28 +0700

    feat: add order module with controller, service, and DTOs

---
+++ src/order/order.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
+import { OrderService } from "./order.service";
+
+@Controller("/api/orders")
+export class OrderController {
+  constructor(private readonly orderService: OrderService) {}
+
+  @HttpCode(HttpStatus.CREATED)
+  @Post()
+  async createOrder() {
+    await this.orderService.createOrder();
+  }
+}
---
+++ src/order/order.service.ts
@@ -0,0 +1,18 @@
+import { Inject, Injectable } from "@nestjs/common";
+import { PrismaService } from "../common/prisma.service";
+import { Logger } from "winston";
+import { WINSTON_MODULE_PROVIDER } from "nest-winston";
+
+@Injectable()
+export class OrderService {
+  constructor(
+    private readonly prismaService: PrismaService,
+    @Inject(WINSTON_MODULE_PROVIDER)
+    private readonly logger: Logger,
+  ) {}
+
+  async createOrder() {
+    this.logger.info(`ORDER_SERVICE.createOrder: ...`);
+    await this.prismaService.$executeRaw``;
+  }
+}
