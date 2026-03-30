c2d91582f106a866969f857ed0c44ca426cb238f
saken78 <sakenspot@gmail.com>
2026-03-30 23:17:51 +0700

    refactor: reorder imports and remove TypeOrmModule from auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,13 @@
 import { Module } from "@nestjs/common";
 import { JwtModule, JwtService } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserStrategy } from "../common/strategies/admin.strategy";
+import { AtStrategy } from "../common/strategies/at.strategy";
+import { JwtStrategy } from "../common/strategies/jwt.strategy";
+import { RtStrategy } from "../common/strategies/rt.strategy";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { jwtConstants } from "./constants";
-import { UserStrategy } from "../common/strategies/admin.strategy";
-import { JwtStrategy } from "../common/strategies/jwt.strategy";
-import { RtStrategy } from "../common/strategies/rt.strategy";
-import { AtStrategy } from "../common/strategies/at.strategy";
 import { AuthRepositorySQL } from "./repository_query/auth.repository";
 import { TokenService } from "./services/token.service";
 
@@ -39,7 +38,6 @@ import { TokenService } from "./services/token.service";
     AuthRepositorySQL,
     TokenService,
     JwtService,
-    TypeOrmModule,
   ],
 })
 export class AuthModule {}
