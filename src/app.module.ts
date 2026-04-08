90309d65c4800e21445b82d0465ef3970fa4eae8
saken78 <sakenspot@gmail.com>
2026-04-08 15:33:50 +0700

    fix(config): add envFilePath to ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,14 +18,16 @@ import { OrderModule } from "./order/order.module";
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: "../.env",
+    }),
     JwtModule.registerAsync({
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         secret: config.get<string>("JWT_SECRET"),
       }),
     }),
-
     ThrottlerModule.forRoot({
       throttlers: [
         {
